eba6b9e36fd657210bf043b2e650a1ad2201e059
Meep3692 <meep3692@gmail.com>
2023-10-26 23:21:29 -0400

    Added event class

---
+++ src/main/java/ca/awoo/fwoabl/Event.java
@@ -0,0 +1,52 @@
+package ca.awoo.fwoabl;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * A simple event system.
+ * @param <T> The type of event to be passed to listeners.
+ */
+public class Event<T> {
+    /**
+     * A listener for an event.
+     */
+    public static interface Listener<T> {
+        /**
+         * Called when an event is invoked.
+         * @param args The event arguments.
+         */
+        public void onEvent(T args);
+    }
+
+    private Set<Listener<T>> listeners = Collections.synchronizedSet(new HashSet<Listener<T>>());
+
+    /**
+     * Add a listener to this event.
+     * @param listener The listener to add.
+     */
+    public void add(Listener<T> listener) {
+        listeners.add(listener);
+    }
+
+    /**
+     * Remove a listener from this event.
+     * @param listener The listener to remove.
+     */
+    public void remove(Listener<T> listener) {
+        listeners.remove(listener);
+    }
+
+    /**
+     * Invoke this event.
+     * @param args The event arguments.
+     */
+    public void invoke(T args) {
+        synchronized(listeners){
+            for (Listener<T> listener : listeners) {
+                listener.onEvent(args);
+            }
+        }
+    }
+}
---
+++ src/test/java/ca/awoo/fwoabl/EventTest.java
@@ -0,0 +1,33 @@
+package ca.awoo.fwoabl;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Test the Event class.
+ */
+public class EventTest {
+    /**
+     * A listener for an event.
+     */
+    public class listener implements Event.Listener<String> {
+        public String lastEvent;
+        public void onEvent(String args) {
+            lastEvent = args;
+        }
+    }
+
+    /**
+     * Test that an event is recieved by a listener.
+     */
+    @Test
+    public void testEvent() {
+        Event<String> event = new Event<String>();
+        listener listener = new listener();
+        event.add(listener);
+        event.invoke("test");
+        assertEquals("Class recieved event", "test", listener.lastEvent);
+    }
+    
+}
